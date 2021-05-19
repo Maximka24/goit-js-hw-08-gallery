f715b598eb795c6aacd3544f81950eb5163c50d9
Kovalenko Makcim <max.kow@yandex.ru>
2021-05-19 20:01:13 +0300

    урок 8
    выполнено дз №8 (дополнительно добавлено закрытие на Esc и в оверлей)

---
+++ gallery-all.js
@@ -0,0 +1,56 @@
+import arrGalleryItems from './gallery-items.js'
+
+
+const refs = {
+	jsGallery: document.querySelector('.js-gallery'),
+	jsLightbox: document.querySelector('.js-lightbox'),
+	btnCloseModal: document.querySelector('.lightbox__button'),
+	lightboxImage: document.querySelector('.lightbox__image'),
+	lightboxOverlay: document.querySelector('.lightbox__overlay'),
+}
+
+const resultEditArrGallery = addNewItemsJsGallery(arrGalleryItems);
+refs.jsGallery.insertAdjacentHTML('beforeend', resultEditArrGallery);
+
+refs.jsGallery.addEventListener('click', onOpenModalClickItemGallery);
+refs.btnCloseModal.addEventListener('click', onCloseModalClickItemGallery);
+refs.lightboxOverlay.addEventListener('click', onCloseModalClickItemGallery);
+
+
+
+
+
+function addNewItemsJsGallery(items) {
+	return items
+		.map(({ preview, original, description }) => {
+			return `
+			<li class="gallery__item">
+				<a class="gallery__link" href="${original}">
+					<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+				</a>
+			</li>`
+		})
+		.join('');
+}
+
+function onOpenModalClickItemGallery(evt) {
+	evt.preventDefault();
+	window.addEventListener('keydown', onCloseModalClickEs);
+
+	refs.jsLightbox.classList.add('is-open');
+
+	refs.lightboxImage.src = evt.target.dataset.source;	
+}
+
+function onCloseModalClickItemGallery() {
+	refs.jsLightbox.classList.remove('is-open');
+	refs.lightboxImage.src = '';
+}
+
+function onCloseModalClickEs(evt) {
+	if(evt.code === 'Escape') {
+		refs.jsLightbox.classList.remove('is-open');
+		refs.lightboxImage.src = '';
+	}
+	window.removeEventListener('keydown', onCloseModalClickEs);
+}
